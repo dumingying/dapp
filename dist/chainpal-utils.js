2738247bf659d73bb595ccf484d8101f64c58a3c
mingyingdu <469192674@qq.com>
2024-02-20 15:51:36 +0800

    fix:修改导出方法

--- dist/chainpal-utils.js
+++ dist/chainpal-utils.js
@@ -1,40 +1,41 @@
 var r = Object.defineProperty;
-var h = (n, e, t) => e in n ? r(n, e, { enumerable: !0, configurable: !0, writable: !0, value: t }) : n[e] = t;
-var i = (n, e, t) => (h(n, typeof e != "symbol" ? e + "" : e, t), t);
-const c = window.navigator.userAgent, s = c.indexOf("ChainMeet") > -1, l = c.indexOf("ChainPalForPC") > -1, w = s ? c.indexOf("en-US") > -1 ? "en-US" : c.indexOf("zh-CH") > -1 ? "zh-CH" : "" : "", d = (n) => {
+var w = (n, e, t) => e in n ? r(n, e, { enumerable: !0, configurable: !0, writable: !0, value: t }) : n[e] = t;
+var o = (n, e, t) => (w(n, typeof e != "symbol" ? e + "" : e, t), t);
+const c = window.navigator.userAgent, s = c.indexOf("ChainMeet") > -1, h = c.indexOf("ChainPalForPC") > -1, l = s ? c.indexOf("en-US") > -1 ? "en-US" : c.indexOf("zh-CH") > -1 ? "zh-CH" : "" : "", d = (n) => {
   if (s) {
-    const e = window.btoa(unescape(window.encodeURIComponent(JSON.stringify(n)))), t = new RegExp("chainmeetShare=[^&]*", "gi"), a = window.location.search.indexOf("chainmeetShare") > -1 ? window.location.search.replace(t, `chainmeetShare=${e}`) : `${window.location.search}${window.location.search ? "&" : "?"}chainmeetShare=${e}`;
-    window.history.replaceState(null, "", a);
+    const e = window.btoa(unescape(window.encodeURIComponent(JSON.stringify(n)))), t = new RegExp("chainmeetShare=[^&]*", "gi"), i = window.location.search.indexOf("chainmeetShare") > -1 ? window.location.search.replace(t, `chainmeetShare=${e}`) : `${window.location.search}${window.location.search ? "&" : "?"}chainmeetShare=${e}`;
+    window.history.replaceState(null, "", i);
   }
 }, p = (n, e) => {
   var t;
   if (s)
     try {
-      const a = {
+      const i = {
         name: n,
         ...e
       };
-      (t = window == null ? void 0 : window.ReactNativeWebView) == null || t.postMessage(JSON.stringify(a));
-    } catch (a) {
-      console.error(a);
+      (t = window == null ? void 0 : window.ReactNativeWebView) == null || t.postMessage(JSON.stringify(i));
+    } catch (i) {
+      console.error(i);
     }
 }, g = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   callNativeMethods: p,
   setChainmeetShare: d
 }, Symbol.toStringTag, { value: "Module" }));
-class o {
+class a {
   //  server
 }
-i(o, "isApp", s), //是否是app 包含pc端
-i(o, "isChainpalPc", l), // 是否是PC端， 仅PC端 
-i(o, "appLanguage", w), //获取App环境语言 , app 外部返回空
-i(o, "client", g);
-window.ChainPal = o;
+o(a, "isApp", s), //是否是app 包含pc端
+o(a, "isChainpalPc", h), // 是否是PC端， 仅PC端 
+o(a, "appLanguage", l), //获取App环境语言 , app 外部返回空
+o(a, "client", g);
+window.ChainPal = a;
+const u = new a();
 export {
-  w as appLanguage,
+  l as appLanguage,
   g as client,
-  o as default,
+  u as default,
   s as isApp,
-  l as isChainpalPc
+  h as isChainpalPc
 };
