10ff7a4f33d91564bb00d759ca8eb4288eb5b792
mingyingdu <469192674@qq.com>
2024-02-20 16:17:46 +0800

    fix:去掉文件

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,25 +1,9 @@
 /// <reference types="vite/client" />
-
-/*
- * Window type extension to support ReactNativeWebView
- */
-
 declare interface Window {
 	ReactNativeWebView?: any,
 	[ChainPalJS: string]: any
 }
-
-// declare namespace ChainPalJS {
-// 	/**
-// 	 * 生成数字范围内的随机数
-// 	 * @param min 最小数字
-// 	 * @param max 最大数字
-// 	 * @returns number类型
-// 	 */
-// 	export function random(min: number, max: number): number
-// }
-
-declare module 'chainpal-js' {
+declare module 'chainpal-utils' {
 	export = ChainPalJS
 }
 
