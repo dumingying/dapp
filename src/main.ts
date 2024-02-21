cc6e841c8452dac967549f83f359865e64622e3e
mingyingdu <469192674@qq.com>
2024-02-21 16:25:55 +0800

    fix:修改文件

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
-import ChainPal, { appLanguage, setChainmeetShare } from '../lib/main'
-console.log(ChainPal, appLanguage, setChainmeetShare)
+import { appLanguage, setChainmeetShare } from '../lib/main'
+console.log(appLanguage, setChainmeetShare)
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="container">
   链上会app 插件管理 对外提供的接入方法
