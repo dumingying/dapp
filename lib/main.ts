ccf133034bdef9e7b8981b3d1cef1dd2281d347a
mingyingdu <469192674@qq.com>
2024-02-23 14:01:32 +0800

    fix:完善文件

--- lib/main.ts
+++ lib/main.ts
@@ -3,7 +3,7 @@ import { isApp, isChainpalPc, appLanguage } from './common'
 import { callNativeMethods, setChainmeetShare } from "./client" // 客户端方法
 // import * as client from "./client" // 客户端方法
 // import * as server from "./server" // 服务端接口 
-export {
+module.exports = {
 	isApp,
 	isChainpalPc,
 	appLanguage,
